b28aa2b7185d7c3f4cde3065a2ce83d5b384bdc5
Brianna Renni <briannaxrene@icloud.com>
2022-10-27 19:34:21 -0700

    Add Customer class

--- customer-orders/Customer.cs
+++ customer-orders/Customer.cs
@@ -4,17 +4,30 @@ namespace NewUserApp {
 
     class Customer {
         // Instance Fields (private by default)
+        public string name {get; set;}
+        public int orderId {get; set;}
+        public string destination {get; set;}
+        public double totalPaid {get; set;}
 
         // Static field - every object shares
+        public static bool orderPaid = true;
 
         // Constructor method
-
-
-        // Instance methods, belongs to objects
-        public void PrintMessage() {
-
+        public Customer(string fullName, int orderNumber, string city, double orderTotal) {
+            this.name = fullName;
+            this.orderId = orderNumber;
+            this.destination = city;
+            this.totalPaid = orderTotal;
         }
 
+        // Instance methods, belongs to objects
+        public void ShowConfirmation() {
+            LoadingMessage();
+            Console.WriteLine(name);
+            Console.WriteLine($"Shipping to: {destination}");
+            Console.WriteLine($"Order Total: {totalPaid}");
+            Console.WriteLine($"Order number: {orderId}");
+        }
 
         // Static methods - belong to class
         public static void LoadingMessage() {
--- customer-orders/Program.cs
+++ customer-orders/Program.cs
@@ -5,9 +5,7 @@ namespace NewUserApp {
    class Program {
       public static void Main(String[] args) {
 
-         Admin dev1 = new Admin("Hari Smith", 101, true, 18.00);
 
-         Customer user1 = new Customer("Bojack Nicholas", 302, );
 
       }
    }

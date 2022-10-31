6da1e8b9247cf39ee5b4c5bd056576a7fbc608c8
Brianna Renni <briannaxrene@icloud.com>
2022-10-31 13:55:33 -0700

    Fix Program.cs stopping at user input

--- GameCollection/HotNCold/Program.cs
+++ GameCollection/HotNCold/Program.cs
@@ -4,19 +4,21 @@ namespace HotNCold {
     public class Program {
         public static void Main(string[] args) {
             Guessing game = new Guessing();
+
             Console.WriteLine("Number Guesser started:"); // greet the user
 
-
             int secretNum = game.GenerateSecretNum();
+
             int userNum = 0;
 
             do {
                 userNum = game.GetUserNum();
-                game.PrintResult(secretNum, userNum);
+
+                Console.WriteLine(game.PrintResult(secretNum, userNum));
 
             } while (secretNum != userNum);
-            Console.WriteLine("Thanks for playing!");
 
+            Console.WriteLine("Thanks for playing!");
         }
     }
 }

87e189515ac132fc39a5d46b56097facaba82135
Brianna Renni <briannaxrene@icloud.com>
2022-11-01 10:29:04 -0700

    Add Gamev2.cs for refactor

---
+++ RockPaperScissors/Gamev2.cs
@@ -0,0 +1,16 @@
+using System;
+// TODO: Computer choice should be dict
+// TODO: Take input, lowercase, trim, save
+// TODO: Refactor for string, not int
+
+namespace RPSGame {
+    public class Gamev2 {
+        public Gamev2() {
+
+
+
+
+
+        }
+    }
+}

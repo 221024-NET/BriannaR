b8678fa6efc60356965ef08827d7d6a9a9909007
Brianna Renni <briannaxrene@icloud.com>
2022-11-01 15:54:00 -0700

    Add Gamev2.cs for refactor and sln

---
+++ RockPaperScissors/GameV2.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace RPSGame {
+    public class GameV2 {
+        // Fields
+        // TODO: add choice dict
+
+        public GameV2() { }
+
+        // TODO: getCompNum()
+
+        // TODO: getUserNum()
+
+        // TODO: inputValidation()
+
+        // TODO: getChoice()
+        // Should take int and return str choice from dict
+
+        // TODO: getResult()
+
+        // ? printResult()
+
+        // TODO: playAgainPrompt()
+
+
+
+
+
+    }
+
+}

9c1f48f7f170d236dd46119abd8ee8350969677a
Brianna Renni <briannaxrene@icloud.com>
2022-11-02 11:24:39 -0700

    Add test to Program.cs

--- CollectionsTest/CollectionsTestApp/ListTest.cs
+++ CollectionsTest/CollectionsTestApp/ListTest.cs
@@ -1,17 +1,23 @@
 namespace CollectionsApp {
     public class Lists {
         // Fields
-        List<int> bigList = new List<int>(1000);
+        List<int> bigList = new List<int>(500);
+        // Random rand = new Random();
 
 
         public Lists() {
             for (int i = 0; i < this.bigList.Count; i++) {
                 bigList.Insert(i, i * 5);
             }
+
+            // for (int i = 0; i < this.bigList.Count; i++) {
+            //     bigList.Add(rand.Next(501));
+            // }
         }
 
-
         // Methods
+
+
 
 
 
--- CollectionsTest/CollectionsTestApp/Program.cs
+++ CollectionsTest/CollectionsTestApp/Program.cs
@@ -1,13 +1,20 @@
-﻿using System;
-
+﻿
 namespace CollectionsApp {
+
     public class Program {
+
         public static void Main() {
-            Console.WriteLine("Starting!");
+            Console.WriteLine("Starting Timer!");
             Timer myTimer = new Timer();
 
             TimeSpan runTime = myTimer.Run();
             Console.WriteLine("Total elapsed time is: {0} ms", runTime.TotalMilliseconds);
+
+            Lists newList = new Lists();
+            Console.WriteLine($"Count: + {newList.Count}");
+
+            // Console.WriteLine(newList.getNumAtIndex(95));
+
         }
     }
 }

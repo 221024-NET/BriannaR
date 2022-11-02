b51e4ef8485477402f9aa6b176657b954b19f19e
Brianna Renni <69635579+briannarenni@users.noreply.github.com>
2022-11-02 10:04:29 -0700

    Add List.cs

--- CollectionsTest/CollectionsTestApp/ListTest.cs
+++ CollectionsTest/CollectionsTestApp/ListTest.cs
@@ -1,10 +1,22 @@
 namespace CollectionsApp {
     public class Lists {
         // Fields
+        List<int> bigList = new List<int>(1000);
+
 
         public Lists() {
-            //
+            for(int i = 0; i < this.bigList.count; i++) {
+                bigList.Insert(i, i * 5);
+            }
         }
+
+
+        // Methods
+
+
+
+
+
 
 
     }

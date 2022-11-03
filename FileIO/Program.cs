40ce80843478953cab26b85f94334a924770c960
Brianna Renni <briannaxrene@icloud.com>
2022-11-03 11:21:32 -0700

    Add User.cs and fields

---
+++ FileIO/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Wordle {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+            string fpath = "./testFile.txt";
+            string[] words = File.ReadAllLines(fpath);
+            var rand = new Random();
+            //random word from the list
+            string secret = words[rand.Next(words.Length)].ToLower().Trim();
+            bool solved = false;
+            int turns = 0;
+            string guess;
+            //potential array of past guesses to be displayed on every turn's end
+            //ColorString[] guesses = new ColorString[6];
+            ConsoleColor[] feedback = { ConsoleColor.Gray, ConsoleColor.Gray, ConsoleColor.Gray, ConsoleColor.Gray, ConsoleColor.Gray };
+
+            //game loop
+            while (turns < 6) {
+                Console.WriteLine("Guess: ");
+                guess = Console.ReadLine().ToLower();
+                if (guess == null || guess.Length != 5) {
+                    Console.WriteLine("Invalid guess. Must be a 5-letter word");
+                    continue;
+                }//end of if
+                //guesses[turns].S=guess;
+                turns++;
+
+                //wrong guess, give feedback
+                for (int i = 0; i < 5; i++) {
+                    //guessed letter in the right place
+                    if (guess[i] == secret[i]) {
+                        Console.WriteLine("This {0} is in the right place", guess[i]);
+                        feedback[i] = ConsoleColor.Green;
+                        //guess[turns].setCharColor(ConsoleColor.Green,i);
+                    }
+
+                    //guessed letter wrong
+                    //letter is or isn't in the word
+                    else {
+                        Console.WriteLine("The word {0} contain {1}", secret.Contains(guess[i]) ? "does" : "does not", guess[i]);
+                        feedback[i] = secret.Contains(guess[i]) ? ConsoleColor.Yellow : ConsoleColor.Gray;
+                        /*
+                        if secret.Contains(guess[i]) guesses[turns].setCharColor(ConsoleColor.Yellow,i);
+                        //default color is gray so no need to set there
+                        */
+                    }
+                }//end of for
+                //print the colored guess
+                /*If ColorString works just use the given method instead*/
+                for (int i = 0; i < 5; i++) {
+                    printcolor(guess[i], feedback[i]);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.Write("\n");
+
+                //correct guess
+                if (guess == secret) {
+                    solved = true;
+                    break;
+                }//end of if
+            }//end of while
+
+            Console.Write(solved ? "Solved " : "Not solved ");
+            Console.Write("within {0} turn(s). The word was: {1}", turns, secret);
+        }
+
+        //Print a char with a specific background color
+        private static void printcolor(char c, ConsoleColor cc) {
+            Console.BackgroundColor = cc;
+            Console.Write("{0}", c);
+        }
+    }
+
+}
---
+++ FileIO/User.cs
@@ -0,0 +1,20 @@
+namespace Wordle {
+    public class User {
+        //Fields
+        string userName;
+        string password;
+        int wins;
+        int losses;
+        double avgTurns;
+        int[] turns;
+
+        // Constructor
+        public User() {
+
+        }
+
+        // Methods
+
+
+    }
+}

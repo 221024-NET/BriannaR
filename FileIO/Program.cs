3c4b2b82cea265edc8f4bef7bb8d79fa2801ef4d
Brianna Renni <briannaxrene@icloud.com>
2022-11-03 14:30:25 -0700

    Add game methods and file write

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -1,68 +1,115 @@
-﻿namespace Wordle {
+﻿using System;
+using System.Text;
+
+namespace Wordle {
 
     public class Program {
 
         public static void Main(string[] args) {
             string fpath = "./testFile.txt";
+            string gameHistory = "./.playerHistory";
+
             string[] words = File.ReadAllLines(fpath);
             var rand = new Random();
-            //random word from the list
-            string secret = words[rand.Next(words.Length)].ToLower().Trim();
+
             bool solved = false;
-            int turns = 0;
+            int turns;
             string guess;
-            //potential array of past guesses to be displayed on every turn's end
+
+            //potential array of past guesses to be shown on every turn's end
+
             //ColorString[] guesses = new ColorString[6];
             ConsoleColor[] feedback = { ConsoleColor.Gray, ConsoleColor.Gray, ConsoleColor.Gray, ConsoleColor.Gray, ConsoleColor.Gray };
 
-            //game loop
-            while (turns < 6) {
-                Console.WriteLine("Guess: ");
-                guess = Console.ReadLine().ToLower();
-                if (guess == null || guess.Length != 5) {
-                    Console.WriteLine("Invalid guess. Must be a 5-letter word");
-                    continue;
-                }//end of if
-                //guesses[turns].S=guess;
-                turns++;
+            // Create new user
+            Console.WriteLine("Please enter a username: ");
+            string newUsername = Console.ReadLine();
+            Console.WriteLine("Please enter a password: ");
+            string newPassword = Console.ReadLine();
 
-                //wrong guess, give feedback
-                for (int i = 0; i < 5; i++) {
-                    //guessed letter in the right place
-                    if (guess[i] == secret[i]) {
-                        Console.WriteLine("This {0} is in the right place", guess[i]);
-                        feedback[i] = ConsoleColor.Green;
-                        //guess[turns].setCharColor(ConsoleColor.Green,i);
+            User player = new User(newUsername, newPassword);
+
+            bool loop = true;
+
+            // * START GAME LOOP
+            do {
+                turns = 0;
+                string secret = words[rand.Next(words.Length)].ToLower().Trim();
+
+                // ! display secret word for testing
+                Console.WriteLine(secret);
+
+                // Game Loop
+                while (turns < 6) {
+                    Console.WriteLine("Guess: ");
+                    guess = Console.ReadLine().ToLower();
+                    if (guess == null || guess.Length != 5) {
+                        Console.WriteLine("Invalid guess. Must be a 5-letter word");
+                        continue;
+                    }
+                    // guesses[turns].S=guess;
+                    turns++;
+
+                    // wrong guess, give feedback
+                    for (int i = 0; i < 5; i++) {
+                        //guessed letter in the right place
+                        if (guess[i] == secret[i]) {
+                            Console.WriteLine("This {0} is in the right place", guess[i]);
+                            feedback[i] = ConsoleColor.Green;
+                            //guess[turns].setCharColor(ConsoleColor.Green,i);
+                        }
+
+                        //guessed letter wrong
+                        //letter is or isn't in the word
+                        else {
+                            Console.WriteLine("The word {0} contain {1}", secret.Contains(guess[i]) ? "does" : "does not", guess[i]);
+                            feedback[i] = secret.Contains(guess[i]) ? ConsoleColor.Yellow : ConsoleColor.Gray;
+                            /*
+                            if secret.Contains(guess[i]) guesses[turns].setCharColor(ConsoleColor.Yellow,i);
+                            //default color is gray so no need to set there
+                            */
+                        }
                     }
 
-                    //guessed letter wrong
-                    //letter is or isn't in the word
-                    else {
-                        Console.WriteLine("The word {0} contain {1}", secret.Contains(guess[i]) ? "does" : "does not", guess[i]);
-                        feedback[i] = secret.Contains(guess[i]) ? ConsoleColor.Yellow : ConsoleColor.Gray;
-                        /*
-                        if secret.Contains(guess[i]) guesses[turns].setCharColor(ConsoleColor.Yellow,i);
-                        //default color is gray so no need to set there
-                        */
+                    //print the colored guess
+                    /*If ColorString works just use the given method instead*/
+                    for (int i = 0; i < 5; i++) {
+                        printcolor(guess[i], feedback[i]);
+                        Console.BackgroundColor = ConsoleColor.Black;
                     }
-                }//end of for
-                //print the colored guess
-                /*If ColorString works just use the given method instead*/
-                for (int i = 0; i < 5; i++) {
-                    printcolor(guess[i], feedback[i]);
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write("\n");
+
+                    //correct guess
+                    if (guess == secret) {
+                        solved = true;
+                        break;
+                    }
+                }//end of while
+
+
+                Console.WriteLine(solved ? $"Solved in: {0} turn(s)" : "Not solved ");
+                Console.WriteLine("The word was: {1}", turns, secret);
+
+                // Update user info
+                // (win or loss boolean, # of turns int)
+                player.UpdateRecord(solved, turns);
+
+                Console.WriteLine("Play Again? (Y/N)");
+                string choice = Console.ReadLine();
+
+                if (choice.ToUpper() != "Y") {
+                    loop = false;
                 }
-                Console.Write("\n");
 
-                //correct guess
-                if (guess == secret) {
-                    solved = true;
-                    break;
-                }//end of if
-            }//end of while
+            } while (loop);
+            // * END GAME LOOP
 
-            Console.Write(solved ? "Solved " : "Not solved ");
-            Console.Write("within {0} turn(s). The word was: {1}", turns, secret);
+            // (Display player history info)
+            player.SaveHistory(gameHistory);
+            Console.WriteLine("Here are your current stats: ");
+            var playerRecord = player.DisplayRecord(gameHistory);
+            Console.WriteLine(playerRecord);
+
         }
 
         //Print a char with a specific background color
--- FileIO/User.cs
+++ FileIO/User.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Text;
+using System.IO;
+
 namespace Wordle {
     public class User {
         //Fields
@@ -8,13 +12,66 @@ namespace Wordle {
         double avgTurns;
         int[] turns;
 
-        // Constructor
-        public User() {
+        // Constructors
+        public User(string userName, string password) {
+            this.userName = userName;
+            this.password = password;
+            this.wins = 0;
+            this.losses = 0;
+            this.avgTurns = 0;
+            this.turns = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+        }
+
+        public User(string userName, string password, int wins, int losses, double avgTurns, int[] turns) {
+            this.userName = userName;
+            this.password = password;
+            this.wins = wins;
+            this.losses = losses;
+            this.avgTurns = avgTurns;
+            this.turns = turns;
 
         }
 
         // Methods
+        public void UpdateRecord(bool win, int turn) {
+            if (win) {
+                this.wins++;
+                this.turns[turn]++;
+                this.avgTurns = GetAvgTurns(turn);
+            } else
+                this.losses++;
+        }
+
+        public int GetAvgTurns(int turn) {
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < 6; i++) {
+                sum += turns[i] * i;
+                count += turns[i];
+            }
+            return sum / count;
+        }
+
+        public string DisplayRecord(string path) {
+            string[] records = File.ReadAllLines(path);
+            StringBuilder result = new StringBuilder();
+            result.AppendLine("Player \t\t Wins \t\t Losses \t\t Average turns");
+
+            foreach (string record in records) {
+                string[] current = record.Split(", ");
+
+                result.AppendLine($"{current[0]} \t {current[2]} \t\t {current[3]}\t\t\t {current[4]}");
+
+            }
+            return result.ToString();
+        }
 
 
+        public void SaveHistory(string path) {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.userName}, {this.password}, {this.wins}, {this.losses}, {this.avgTurns}, {this.turns[1]}, {this.turns[2]}, {this.turns[3]}, {this.turns[4]}, {this.turns[5]}, {this.turns[6]}");
+            File.AppendAllText(path, sb.ToString());
+        }
     }
 }
